6e2afe8c8c2f372d4de20d32d3c1159bc172f77a
z2_ <88509734+z2-2z@users.noreply.github.com>
2025-07-01 12:54:14 +0200

    Updated dates

--- src/posts/metadata.rs
+++ src/posts/metadata.rs
@@ -108,18 +108,18 @@ impl PostDate {
     
     pub fn month_name(&self) -> &'static str {
         match self.month {
-            1 => "jan.",
-            2 => "feb.",
-            3 => "mar.",
-            4 => "apr.",
+            1 => "jan",
+            2 => "feb",
+            3 => "mar",
+            4 => "apr",
             5 => "may",
-            6 => "jun.",
-            7 => "jul.",
-            8 => "aug.",
-            9 => "sep.",
-            10 => "oct.",
-            11 => "nov.",
-            12 => "dec.",
+            6 => "jun",
+            7 => "jul",
+            8 => "aug",
+            9 => "sep",
+            10 => "oct",
+            11 => "nov",
+            12 => "dec",
             _ => unreachable!(),
         }
     }
